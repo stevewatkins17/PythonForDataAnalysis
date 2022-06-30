53b8f98217616a5e1c69a29e89dfa20f60995232
Steve Watkins <stevewatkins@Steves-MBP.attlocal.net>
2022-06-29 23:25:41 -0400

    functionize data generator

--- data_generator.py
+++ data_generator.py
@@ -1,16 +1,26 @@
 import random
 
-x = 20000
-maincourse = ["pasta" ,"pizza" ,"falafel" ,"pesce con vivo" ,"hasenpfeffer" ,"green coconut curry" ,"ciopinno"]
-veg = ["potato" ,"carrots" ,"kale" ,"broccali" ,"brussels sprouts" ,"cauliflower" ,"peas"]
-dessert = ["chocolate tort" ,"chili chocolate tart" ,"melopita" ,"saffron kheer" ,"cinnamon brioche" ,"mom's peach cobbler" ,"vanilla ice cream"]
+def create_csv(myfile ,linecount ,list0 ,list1 ,list2):
+    with open(myfile, "w") as file:
+        # Writing data to a file
+        for _ in range(linecount):
+            item0 = '"' + random.choice(list0) + '"'
+            item1 = '"' + random.choice(list1) + '"'
+            item2 = '"' + random.choice(list2) + '"'
+            myline = "||".join([item0 ,item1 ,item2])
+            file.writelines(f"{myline} \n")
+    return 0
 
-with open("Data/orders.csv", "a") as file:
-    # Writing data to a file
-    for _ in range(x):
-        mc = random.choice(maincourse)
-        v = random.choice(veg)
-        d = random.choice(dessert)
-        #order = " ,".join(["\"" + mc + "\"", "\"" + v + "\"","\"" + d + "\""])
-        order = " ,".join([mc ,v ,d])
-        file.writelines(f"{order} \n")
+def main():
+    linecount = 20000
+    maincourse = ["" ,"haggis" ,"Miquel's Pizza" ,"falafel" ,"pesce con vivo" ,"hasenpfeffer" ,"green coconut curry" ,"ciopinno" ,"potato pie"]
+    veg = ["" ,"potato au gratin" ,"Chef John 5-spice carrots" ,"kale" ,"broccali" ,"brussels sprouts" ,"cauliflower" ,"peas" ,"roasted green beans"]
+    dessert = ["" ,"tavuk göğsü" ,"creme brulee" ,"Mizu Shingen Mochi" ,"green chili chocolate tart" ,"melopita" ,"saffron kheer" ,"cinnamon brioche" ,"mom's peach cobbler" ,"cherry ice cream"]
+    myfile = "Data/orders.csv"
+
+    csv_create_result = create_csv(myfile ,linecount ,maincourse ,veg ,dessert)
+    return csv_create_result
+
+if __name__ == '__main__':
+    myresult = main()
+    print(f"csv_create_result={myresult}")
