f6d7a8cdc0cc977a08525a317686904e94b935b6
Steve Watkins <stevewatkins@Steves-MBP.attlocal.net>
2022-11-25 01:13:01 -0500

    fix pathing in files

--- test/test_hide_secrets.py
+++ test/test_hide_secrets.py
@@ -1,6 +1,6 @@
 #from PythonForDataAnalysis.src.hide_secrets import *
 from .. src import hide_secrets
-
+""" use 'touch __init__.py' to make sure init file is in all referenced dirs """
 def test_get_key_env():
     key_value = hide_secrets.get_key_env("DARKSKY_API_KEY")
     assert key_value[4] == "f"
