9f7dbc61cdcf0e95cde60f335413ef3b7f3f4710
Steve Watkins <stevewatkins@Steves-MBP.attlocal.net>
2022-11-28 00:22:02 -0500

    test fn_demo complete  - pytest success

--- test/test_fn_demo.py
+++ test/test_fn_demo.py
@@ -1,7 +1,7 @@
 from .. src import fileHandling as fh
 from .. src import fn_demo as fd
 import pytest
-import requests ,os
+import requests ,os ,collections.abc as ca
 
 @pytest.fixture
 def get_dict_filename():
@@ -10,6 +10,11 @@ def get_dict_filename():
     #return mydict ,myfilepathname
     yield mydict ,myfilepathname
     fh.DeleteFile(myfilepathname)
+
+@pytest.fixture
+def create_json_from_dict(get_dict_filename):
+    returncode = fd.create_json_from_dict(get_dict_filename[0] ,get_dict_filename[1])
+    return returncode
 
 @pytest.fixture
 def get_darksky_key():
@@ -47,14 +52,38 @@ def test_get_darksky_url(get_darksky_url):
     if get_darksky_url != None:
         assert get_darksky_url[0:23] == "https://api.darksky.net"
 
-def test_create_json_from_dict(get_dict_filename):
-    import collections.abc
-    returncode = fd.create_json_from_dict(get_dict_filename[0] ,get_dict_filename[1])
-    print(type(get_dict_filename[0]))
-    assert returncode == 0
+def test_create_json_from_dict(get_dict_filename ,create_json_from_dict):
+    assert create_json_from_dict == 0
     assert os.path.isfile(get_dict_filename[1]) == True
 
     """ we test that obj is dictionary """
     assert isinstance(get_dict_filename[0], dict) == True
     #assert True == False
 
+def test_convert_json_to_dict(get_dict_filename ,create_json_from_dict):
+    """Parse a json file and convert to dictionary object"""
+    if create_json_from_dict == 0:
+        my_new_dict = fd.convert_json_to_dict(get_dict_filename[1])
+        print(get_dict_filename[1])
+        print(my_new_dict)
+        
+        assert os.path.isfile(get_dict_filename[1]) == True
+        assert isinstance(my_new_dict, dict) == True
+        assert my_new_dict["mykey"] == "myvalue"
+        #assert 1 == 0
+    
+def test_do_nothing_yet():
+    returncode = fd.do_nothing_yet()
+    assert returncode == None
+
+def test_get_darksky_daily_weather(get_darksky_url):
+    """ return weather data as dict and formatted string """
+    daily_weather ,daily_weather_json = fd.get_darksky_daily_weather(get_darksky_url)
+    #print(type(daily_weather))   
+    print(daily_weather_json)
+    assert daily_weather["timezone"] == "America/Kentucky/Louisville"
+    #assert 1 == 0
+
+def test_main():
+    returncode = fd.main()
+    assert returncode == 0
