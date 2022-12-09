363b88589d78cb797d8c88211c2635758aa417b4
Steve Watkins <stevewatkins@Steves-MBP.attlocal.net>
2022-12-08 23:30:13 -0500

    SQL demo with hubway DB

--- src/SQL_demo_20180925.py
+++ src/SQL_demo_20180925.py
@@ -1,357 +1,210 @@
-
-# coding: utf-8
-
-# ## https://www.dataquest.io/blog/sql-basics/
-# 
-# ### Covering
-#  - SELECT
-#  - WHERE
-#  - GROUP BY
-#  - JOIN
-#  - Conditionals
-
-# In[1]:
-
-
+# SQL_demo_20180925.py
 import os
-os.getcwd()
-# os.chdir("/Users/stevewatkins/python3/demos/sqlite") # relative path: scripts dir is under Users/stevewatkins
-
-
-# ## create conn. and "read results" pd function
-
-# In[2]:
-
-
 import sqlite3
 import pandas as pd
+import urllib.request
 
-db = sqlite3.connect("hubway.db")
+#hubway.db: "https://www.dataquest.io/blog/large_files/hubway.db"
+def get_hubway_db(hubway_db_file):
+  if not os.path.isfile(hubway_db_file):
+    urllib.request.urlretrieve("https://www.dataquest.io/blog/large_files/hubway.db", hubway_db_file)
+  db = sqlite3.connect(hubway_db_file)
+  return db
 
-def run_query(query):
+def run_query(query ,db):
     return pd.read_sql_query(query,db)
 
+def get_top_trips(toplimit ,db):
+  top_trips_sql = f"SELECT * FROM trips LIMIT {toplimit};"
+  top_trips_results = run_query(top_trips_sql ,db)
+  return top_trips_results
+
+def get_table_metadata(db):
+  get_table_metadata_sql = """
+  SELECT * 
+  FROM sqlite_master
+  ORDER BY name;
+  """
+  metadata_results = run_query(get_table_metadata_sql ,db) 
+  return metadata_results
+
+def get_top_duration(db ,mylimit):
+  top_duration_sql = f"""
+  SELECT 
+      duration
+      ,start_date 
+  FROM trips 
+  ORDER BY duration DESC ,start_date ASC
+  LIMIT {mylimit};
+  """
+  top_duration_results = run_query(top_duration_sql ,db) 
+  return top_duration_results
+
+def get_highest_avg_duration_by_station(db ,mylimit):
+  highest_avg_duration_by_station_sql = f"""
+  SELECT s.station , avg(t.duration) as avg_duration 
+  FROM trips t 
+  join stations s on t.start_station = s.id
+  group by s.station
+  order by 2 desc
+  LIMIT {mylimit};
+  """
+  highest_avg_duration_by_station_results = run_query(highest_avg_duration_by_station_sql ,db) 
+  return highest_avg_duration_by_station_results
+
+def get_hubway_query_store():
+
+  duration_GT_9990 = '''
+  SELECT * 
+  FROM trips
+  WHERE duration > 9990;
+  '''
+
+  duration_GTEQ_9990_registered = '''
+  SELECT * 
+  FROM trips
+  WHERE (duration >= 9990) AND (sub_type = "Registered")
+  ORDER BY duration DESC;
+  '''
+
+  Total_Trips_Registered = '''
+  SELECT 
+    COUNT(*) AS "Total Trips by Registered Users"  
+  FROM trips
+  WHERE sub_type = "Registered";
+  '''
+
+  Total_Trips_sub_type = '''
+  SELECT 
+    sub_type
+    ,COUNT(*) AS "Total Trips by Registered Users"  
+  FROM trips
+  group by sub_type
+  order by 2 desc;
+  '''
+
+  trip_avg_duration = '''
+  SELECT sub_type, AVG(duration) AS "Average Duration"
+  FROM trips
+  GROUP BY sub_type;
+  '''
+
+  count_bike_trips = '''
+  SELECT bike_number as "Bike Number", COUNT(*) AS "Number of Trips"
+  FROM trips
+  GROUP BY bike_number
+  ORDER BY COUNT(*) DESC
+  LIMIT 1;
+  '''
+
+  avg_duration_GT_30 = '''
+  SELECT AVG(duration)
+  FROM trips
+  WHERE (2022 - birth_date) > 30;
+  '''
+
+  age_demographics = '''
+    SELECT 
+    birth_date  as "birth year"
+    ,(2017 - birth_date) as "Years Old"
+    ,date('now') as "now date"
+    ,(strftime('%Y',date('now'))) as "now Year string"
+    ,((strftime('%Y',date('now'))) - birth_date) as "Years Old revised"
+    FROM trips
+    WHERE (2018 - birth_date) > 30
+    and birth_date <> '';
+    '''
+
+  mega_join = '''
+  SELECT s.* ,t.*
+  FROM trips t
+  INNER JOIN stations s ON t.start_station = s.id
+  ORDER BY 1 DESC
+  LIMIT 5;
+  '''
 
-# ## read metadata about sqlite DB tables
+  return duration_GT_9990 ,duration_GTEQ_9990_registered ,Total_Trips_Registered ,Total_Trips_sub_type ,trip_avg_duration ,count_bike_trips ,avg_duration_GT_30 ,age_demographics ,mega_join
 
-# In[3]:
+def create_mytable():
+  create_table_sql = '''
+  CREATE TABLE IF NOT EXISTS MyTable(rid integer);
+  '''
 
+  cn2 = sqlite3.connect("../data/MySqlite.DB")
 
-query = """
-SELECT * 
-FROM sqlite_master
-ORDER BY name;
-"""
-run_query(query)
+  c = cn2.cursor()
 
+  c.execute(create_table_sql)
 
-# ## select all from table
+  MyQuery = """
+  SELECT * 
+  FROM sqlite_master
+  ORDER BY name;
+  """
 
-# In[4]:
+  c.execute(MyQuery)
 
+  c.close()
 
-query = "SELECT * FROM trips LIMIT 5;"
-run_query(query)
+  return 0
 
+def insert_mytable():
+  MyInsert = 'insert into MyTable values(-199) ,(17);'
 
-# ## select 2 columns from table with custom order
+  cn2 = sqlite3.connect("../data/MySqlite.DB")
 
-# In[5]:
+  c = cn2.cursor()
 
+  c.execute(MyInsert)
+  cn2.commit()
+  c.close()
 
-query = """
-SELECT 
-     duration
-    ,start_date 
-FROM trips 
-ORDER BY duration DESC ,start_date ASC
-LIMIT 5;
-"""
-run_query(query)
+  pd.read_sql_query("select * from MyTable;",cn2)
 
+def update_mytable():
+  MyUpdate = 'update MyTable set rid = 0 where rid = -199;'
 
-# ## select all where duration is max, using limit & order desc
+  cn2 = sqlite3.connect("../data/MySqlite.DB")
+  c = cn2.cursor()
 
-# In[6]:
+  c.execute(MyUpdate)
 
+  cn2.commit()
 
-query = '''
-SELECT duration ,*
-FROM trips
-ORDER BY duration DESC
-LIMIT 1;
-'''
+  c.close()
 
-run_query(query)
+  pd.read_sql_query("select * from MyTable;",cn2)
 
+def delete_mytable():
+  MyDelete = 'delete from MyTable where rid = 0;'
 
-# ## select max duration per each start_station, using group by
+  cn2 = sqlite3.connect("../data/MySqlite.DB")
+  c = cn2.cursor()
 
-# In[7]:
+  c.execute(MyDelete)
 
+  cn2.commit()
 
-query = """
-SELECT max(duration) ,start_station 
-FROM trips
-group by start_station
-order by 1 desc
-limit 5;
+  c.close()
 
-"""
+  pd.read_sql_query("select * from MyTable;",cn2)
 
-run_query(query)
 
+def main():
+  hubway_db_file = "../data/hubway.db"
+  db = get_hubway_db(hubway_db_file)
 
-# ## where clause to filter resultset
+  metadata_results = get_table_metadata(db)
+  top_trips_results = get_top_trips(3,db)
+  top_duration_results = get_top_duration(db,3)
+  highest_avg_duration_by_station_results = get_highest_avg_duration_by_station(db,3)
 
-# In[8]:
+  print(metadata_results)
+  print(top_trips_results)
+  print(top_duration_results)
+  print(highest_avg_duration_by_station_results)
 
+  return 0
 
-query = '''
-SELECT * 
-FROM trips
-WHERE duration > 9990
---limit 5
-;
-'''
-
-run_query(query)
-
-
-# # AND condition
-
-# In[9]:
-
-
-query = '''
-SELECT * 
-FROM trips
-WHERE (duration >= 9990) AND (sub_type = "Registered")
-ORDER BY duration DESC;
-'''
-
-run_query(query)
-
-
-# # Count with AND condition
-
-# In[10]:
-
-
-query = '''
-SELECT 
-  COUNT(*) AS "Total Trips by Registered Users"  
-FROM trips
-WHERE sub_type = "Registered";
-'''
-
-run_query(query)
-
-
-# # Aggregating data with GROUP BY
-
-# In[11]:
-
-
-query = '''
-SELECT 
-   sub_type
-  ,COUNT(*) AS "Total Trips by Registered Users"  
-FROM trips
-group by sub_type
-order by 2 desc;
-'''
-
-run_query(query)
-
-
-# In[12]:
-
-
-query = '''
-SELECT sub_type, AVG(duration) AS "Average Duration"
-FROM trips
-GROUP BY sub_type;
-'''
-
-run_query(query)
-
-
-# # which bike was used for the most trips?
-
-# In[13]:
-
-
-query = '''
-SELECT bike_number as "Bike Number", COUNT(*) AS "Number of Trips"
-FROM trips
-GROUP BY bike_number
-ORDER BY COUNT(*) DESC
-LIMIT 1;
-'''
-
-run_query(query)
-
-
-# # Arithmetic Operators
-
-# In[14]:
-
-
-query = '''
-SELECT AVG(duration)
-FROM trips
-WHERE (2018 - birth_date) > 30;
-'''
-
-run_query(query)
-
-
-# In[15]:
-
-
-query = '''
-SELECT 
- birth_date  as "birth year"
-,(2017 - birth_date) as "Years Old"
-,date('now') as "now date"
-,(strftime('%Y',date('now'))) as "now Year string"
-,((strftime('%Y',date('now'))) - birth_date) as "Years Old revised"
-FROM trips
-WHERE (2018 - birth_date) > 30
-and birth_date <> '';
-'''
-
-run_query(query)
-
-
-# # Joins
-
-# In[16]:
-
-
-query = '''
-SELECT *
-FROM stations
-where id = 23
-LIMIT 5;
-'''
-run_query(query)
-
-
-# In[17]:
-
-
-query = '''
-SELECT *
-FROM trips
-where start_station = 23 -- or end_station = 23
-LIMIT 5;
-'''
-run_query(query)
-
-
-# In[18]:
-
-
-query = '''
-SELECT s.* ,t.*
-FROM trips t
-INNER JOIN stations s ON t.start_station = s.id
-and s.id = 23
---GROUP BY s.station
-ORDER BY 1 DESC
-LIMIT 5;
-'''
-
-run_query(query)
-
-
-# # Create new DB & table
-
-# In[19]:
-
-
-#intentional failure; SQL is syntactically correct
-query = '''
-CREATE TABLE IF NOT EXISTS MyTable(rid integer);
-'''
-
-#run_query(query)
-
-
-# In[20]:
-
-
-create_table_sql = '''
-CREATE TABLE IF NOT EXISTS MyTable(rid integer);
-'''
-
-cn2 = sqlite3.connect("MySqlite.DB")
-
-c = cn2.cursor()
-
-c.execute(create_table_sql)
-
-MyQuery = """
-SELECT * 
-FROM sqlite_master
-ORDER BY name;
-"""
-
-c.execute(MyQuery)
-
-c.close()
-
-#pd.read_sql_query(MyQuery,cn2)
-
-
-# # INSERT, UPDATE, DELETE
-
-# In[21]:
-
-
-MyInsert = 'insert into MyTable values(-199) ,(17);'
-
-c = cn2.cursor()
-
-c.execute(MyInsert)
-
-cn2.commit()
-
-c.close()
-
-pd.read_sql_query("select * from MyTable;",cn2)
-
-
-# In[22]:
-
-
-MyUpdate = 'update MyTable set rid = 0 where rid = -199;'
-
-c = cn2.cursor()
-
-c.execute(MyUpdate)
-
-cn2.commit()
-
-c.close()
-
-pd.read_sql_query("select * from MyTable;",cn2)
-
-
-# In[23]:
-
-
-MyDelete = 'delete from MyTable where rid = 0;'
-
-c = cn2.cursor()
-
-c.execute(MyDelete)
-
-cn2.commit()
-
-c.close()
-
-pd.read_sql_query("select * from MyTable;",cn2)
-
+if __name__ == '__main__':
+  myresult = main()
